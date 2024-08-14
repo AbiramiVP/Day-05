f65dfa99dfb1027331b69312f8f362b21d0fcf63
Abirami P <146667433+AbiramiVP@users.noreply.github.com>
2024-08-14 11:55:48 +0530

    Create uber.js

---
+++ uber.js
@@ -0,0 +1,34 @@
+//04) uber price calculater: ₹52.5 base fare, ₹6.3/km 
+    class UberPriceCalculator {
+        constructor(km) { 
+            this.km = km;
+            this.baseFare = 52.5;
+            this.ratePerKm = 6.3;
+        }
+        calculation(){
+        
+            let Rate = this.ratePerKm * this.km;
+            let TripAmout = this.baseFare + Rate;
+
+            return TripAmout; //147
+        }
+
+        getEstimatedTripAmount() {
+            
+            let Totel = this.calculation(); 
+            return `Your Estimated Uber Amount is ₹${Totel.toFixed(2)} for ${this.km} KM's` ;
+            
+        }
+
+    }
+
+    let user1 = new UberPriceCalculator(15);
+
+    console.log(user1.getEstimatedTripAmount());
+    console.log("Thanks for Using Uber Cab Services...!")
+
+
+//output --------------------------------------
+
+// Your Estimated Uber Amount is ₹147.00 for 15 KM's
+// Thanks for Using Uber Cab Services...!
